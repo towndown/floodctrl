71c00db4a6aaa89c747d681ba03ba592c5886c28
towndown <tocka992@gmail.com>
2025-04-06 09:42:34 +0900

    commit

---
+++ src/app/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar(){
+    return <nav className="nav">
+        <a href="/" className="site-title">FLOOD CTRL</a>
+        <ul>
+            <li>
+                <a href="/pricing">Pricing</a>
+                <a href="/about">About</a>
+            </li>
+        </ul>
+    </nav>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return(
-    <div>Hello</div>
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello world</h1>
+      <Link href="/users">Users</Link>
+      </main>
   );
 }
